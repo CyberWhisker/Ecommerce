772458d0848f6549340941d67a888b744ee26351
CyberWhisker <ejlopez0530@gmail.com>
2023-10-18 16:54:57 +0800

    First Project

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    protected function authentication() {
+        if (Auth::user()->role_as = '1') {
+            return redirect('admin/dashboard')->with('status', 'Welcome to DashBoard');
+        } else {
+            return redirect('/')->with('status', 'Logged in');
+        }
+    }
+}
