9f91b0b7c5fa32750f56ee00c8763d4b7575ef0b
CyberWhisker <ejlopez0530@gmail.com>
2023-10-19 09:53:35 +0800

    Interface Ongoing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,32 +17,34 @@
     <!-- endinject -->
     <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
 </head>
-<body>
-    @yield('content')
+<body style="background-color: #e3e3e3;">
     <div class="container-scroller">
-        @include('layouts.inc.admin.navbar')
-        <div class="container-fluid page-body-wrapper">
-            @include('layouts.inc.admin.slidebar')
+        @yield('content')
+        <div class="container-scroller">
+            @include('layouts.inc.admin.navbar')
+            <div class="container-fluid page-body-wrapper">
+                @include('layouts.inc.admin.slidebar')
+            </div>
         </div>
+        <!-- plugins:js -->
+        <script src="{{ asset('admin/vendors/base/vendor.bundle.base.js') }}"></script>
+        <!-- endinject -->
+        <!-- Plugin js for this page-->
+        <script src="{{ asset('admin/vendors/chart.js/Chart.min.js') }}"></script>
+        <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+        <script src="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+        <!-- End plugin js for this page-->
+        <!-- inject:js -->
+        <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+        <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
+        <script src="{{ asset('admin/js/template.js') }}"></script>
+        <!-- endinject -->
+        <!-- Custom js for this page-->
+        <script src="{{ asset('admin/js/dashboard.js') }}"></script>
+        <script src="{{ asset('admin/js/data-table.js') }}"></script>
+        <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
+        <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
+        <!-- End custom js for this page-->
     </div>
-    <!-- plugins:js -->
-    <script src="{{ asset('admin/vendors/base/vendor.bundle.base.js') }}"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page-->
-    <script src="{{ asset('admin/vendors/chart.js/Chart.min.js') }}"></script>
-    <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-    <!-- End plugin js for this page-->
-    <!-- inject:js -->
-    <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('admin/js/template.js') }}"></script>
-    <!-- endinject -->
-    <!-- Custom js for this page-->
-    <script src="{{ asset('admin/js/dashboard.js') }}"></script>
-    <script src="{{ asset('admin/js/data-table.js') }}"></script>
-    <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
-    <!-- End custom js for this page-->
 </body>
 </html>
